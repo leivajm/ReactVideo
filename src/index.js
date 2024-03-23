976e71be195a709e0d35c04657d06c57cc89995c
José Leiva <leiva.ruano@gmail.com>
2024-03-23 09:54:49 -0600

    remover mensajes de consola
    se remueve por ser innecesario

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 //import Header from './components/Header';
 import App from './containers/App';
 
-console.log("Hola mundo")
 
-//ReactDOM.render(<Header />, document.getElementById("app"));
+
 ReactDOM.render(<App />, document.getElementById("app"));
